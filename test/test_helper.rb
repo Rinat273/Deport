67ad6bb583582f37a25d80633806e8dde4f66539
Rinat273 <bairam0487@gmail.com>
2023-10-18 02:15:23 +0400

    Limiting Access

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,8 +2,8 @@ ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
 
-module ActiveSupport
-  class TestCase
+class ActiveSupport::TestCase
+
     # Run tests in parallel with specified workers
     parallelize(workers: :number_of_processors)
 
@@ -11,5 +11,24 @@ module ActiveSupport
     fixtures :all
 
     # Add more helper methods to be used by all tests here...
+end
+class ActionDispatch::IntegrationTest
+  def login_as(user)
+    if respond_to? :visit
+      visit login_url
+      fill_in :name, with: user.name
+      fill_in :password, with: 'secret'
+      click_on 'Login'
+    else
+      post login_url, params: { name: user.name, password: 'secret' }
+    end
+  end
+
+  def logout
+    delete logout_url
+  end
+
+  def setup
+    login_as users(:one)
   end
 end
