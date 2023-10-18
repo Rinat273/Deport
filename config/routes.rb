4f97f7e83bad7a196e155cda5f21f7ff20f06a0a
Rinat273 <bairam0487@gmail.com>
2023-10-19 00:25:16 +0400

    Selecting the Locale

--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,18 @@ Rails.application.routes.draw do
     post 'login' => :create
     delete 'logout' => :destroy
   end
-
   get 'sessions/create'
   get 'sessions/destroy'
+
   resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-  root 'store#index', as: 'store_index'
   resources :products
+
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store_index'
+  end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
