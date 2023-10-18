3dffc759a021d993d4dc37030c07e288dbe2dd1e
Rinat273 <bairam0487@gmail.com>
2023-10-19 02:50:19 +0400

    Translating Checkout

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -3,6 +3,6 @@ I18n.default_locale = :en
 
 LANGUAGES = [
   ['English',                  'en'],
-  ["Espa&ntilde;ol".html_safe, 'es']
+  ["Espa&ntilde;ol".html_safe, 'es'],
   ["Rus&ntilde;ol".html_safe,  'ru']
 ]
